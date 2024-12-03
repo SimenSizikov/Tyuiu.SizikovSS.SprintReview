1e3af787a7f7e5d767dd8a8d7939b44b06a22f53
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-12-04 04:14:26 +0500

    Add project files.

---
+++ Tyuiu.SizikovSS.SprintReview.Sprint2.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SizikovSS.SprintReview.Sprint2.V7.Lib;
+
+namespace Tyuiu.SizikovSS.SprintReview.Sprint2.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new();
+            double x = 0, y = 1;
+
+            Assert.AreEqual(true, ds.CheckDotInShadedArea(x, y));
+        }
+    }
+}
---
+++ Tyuiu.SizikovSS.SprintReview.Sprint2.V7/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using Tyuiu.SizikovSS.SprintReview.Sprint2.V7.Lib;
+
+namespace Tyuiu.SizikovSS.SprintReview.Sprint2.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new();
+
+            Console.Title = "Спринт #2 | Выполнил: Сизиков С. С. | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Спринт-ревью                                                      *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Сизиков С. С. | РППб-24-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные           *");
+            Console.WriteLine("* (вещественные значения) и вычисляет, находится ли точка с координатами  *");
+            Console.WriteLine("* X,Y в заштрихованной области.                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.CheckDotInShadedArea(x, y));
+
+            Console.ReadLine();
+        }
+    }
+}
